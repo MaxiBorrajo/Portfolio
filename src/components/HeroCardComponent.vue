921ce469eecf488056826329ecc6b241d0f27158
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2023-11-02 18:38:26 -0300

    download cv button added

--- src/components/HeroCardComponent.vue
+++ src/components/HeroCardComponent.vue
@@ -3,9 +3,12 @@
     <div class="card__left d-flex justify-center flex-column">
       <p class="card__left__title">Maximiliano Borrajo</p>
       <p class="card__left__subtitle">Backend developer</p>
-      <!-- <ButtonComponent button-type="button"
-      button-label="download cv"
-      button_width="25%"/> -->
+      <ButtonComponent
+        button-type="button"
+        button-label="download cv"
+        @click="downloadCV"
+        class="button-download"
+      />
     </div>
     <div class="card__right d-flex align-center justify-center">
       <img
@@ -27,23 +30,35 @@
 
 <script setup>
 import { ref } from "vue";
+import ButtonComponent from "@/components/ButtonComponent.vue";
 import yo3 from "../assets/yo3.png";
 import yo2 from "../assets/yo2.gif";
 import flecha from "../assets/flecha.png";
-import sound_shield from "../assets/sound_shield.mp3"
+import sound_shield from "../assets/sound_shield.mp3";
+import cv from "../assets/cv.pdf";
 
-const audio = new Audio(sound_shield)
+const audio = new Audio(sound_shield);
 const src = ref(yo3);
 
 function activateAnimation() {
-  audio.currentTime = 0.4
-  audio.play()
+  audio.currentTime = 0.4;
+  audio.play();
   src.value = yo2;
 }
 
 function deactivateAnimation() {
-  audio.pause()
+  audio.pause();
   src.value = yo3;
+}
+
+function downloadCV() {
+  const link = document.createElement("a");
+  link.href = cv;
+  link.target = "_blank";
+  link.download = "Maximiliano Borrajo cv";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 </script>
 
@@ -53,6 +68,12 @@ function deactivateAnimation() {
   border-radius: 20px;
   background-color: rgb(var(--v-theme-card));
   color: white;
+
+  .button-download {
+    margin-top: 10px;
+    width: 120px;
+    font-weight: 700;
+  }
 
   &__left,
   &__right {
@@ -101,34 +122,35 @@ function deactivateAnimation() {
 
 @media only screen and (max-width: 600px) {
   .card {
-    width: 100%!important;
-    height: 100%!important;
+    width: 100% !important;
+    height: 100% !important;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     text-align: center;
-    padding: 30px!important;
+    padding: 30px !important;
     gap: 50px;
     &__left,
     &__right {
-      height: 50%!important;
+      height: 50% !important;
     }
 
     &__left {
-      width: 100%!important;
-      padding: 0px!important;
+      width: 100% !important;
+      padding: 0px !important;
+      align-items: center;
     }
 
     &__right {
-      width: 100%!important;
-      padding: 0!important;
+      width: 100% !important;
+      padding: 0 !important;
       &__text {
-        top: 15px!important;
-        left: -15px!important;
+        top: 15px !important;
+        left: -15px !important;
       }
       &__icon {
-        top: 45px!important;
-        left: -30px!important;
+        top: 45px !important;
+        left: -30px !important;
       }
     }
   }
@@ -136,41 +158,39 @@ function deactivateAnimation() {
 
 @media only screen and (min-width: 601px) and (max-width: 780px) {
   .card {
-flex-grow: 1;
-  &__left {
-    width: 50%;
-    &__title {
-      font-size: 30px;
+    flex-grow: 1;
+    &__left {
+      width: 50%;
+      &__title {
+        font-size: 30px;
+      }
+      &__subtitle {
+        font-size: 15px;
+      }
     }
-    &__subtitle {
-      font-size: 15px;
-    }
-  }
 
-  &__right {
-    width: 50%;
-    
+    &__right {
+      width: 50%;
+    }
   }
-}
 }
 
 @media only screen and (min-width: 781px) and (max-width: 1024px) {
   .card {
-flex-grow: 1;
-  &__left {
-    width: 60%;
-    &__title {
-      font-size: 30px;
+    flex-grow: 1;
+    &__left {
+      width: 60%;
+      &__title {
+        font-size: 30px;
+      }
+      &__subtitle {
+        font-size: 15px;
+      }
     }
-    &__subtitle {
-      font-size: 15px;
-    }
-  }
 
-  &__right {
-    width: 40%;
-    
+    &__right {
+      width: 40%;
+    }
   }
-}
 }
 </style>
